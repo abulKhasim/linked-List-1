55b69fd0ea4f24b7711e8f142cd37780efe193b1
abulKhasim <abulkhasim999@gmail.com>
2023-03-21 21:03:20 +0530

    Initial Commit

---
+++ 19_desgnHashset.cpp
@@ -0,0 +1,62 @@
+class MyHashSet {
+public:
+    vector<int> m;
+    int siz;
+    MyHashSet() {
+        siz = 1e6+1;
+        m.resize(siz);
+    }
+    
+    void add(int key) {
+        m[key] = 1;
+    }
+    
+    void remove(int key) {
+        m[key] = 0;
+    }
+    
+    bool contains(int key) {
+        return m[key];
+    }
+};
+
+
+
+
+
+class MyHashSet {
+public:
+    vector<list<int>> m;
+    int siz;
+    MyHashSet() {
+        siz = 10;
+        m.resize(siz);
+    }
+
+    int hash(int key) {
+        return key % siz;
+    }
+
+    list<int> :: iterator search(int key) {
+        int i = hash(key);
+        return find(m[i].begin(), m[i].end(), key);
+    }
+    
+    void add(int key) {
+        if(contains(key)) return;
+        int i = hash(key);
+        m[i].push_back(key);
+    }
+    
+    void remove(int key) {
+        if (!contains(key)) return;
+        int i = hash(key);
+        m[i].erase(search(key));
+    }
+    
+    bool contains(int key) {
+        int i = hash(key);
+        if (search(key) != m[i].end()) return true;
+        return false;
+    }
+};
